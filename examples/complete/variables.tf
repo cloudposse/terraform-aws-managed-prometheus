07b7f75161f1407c7c802c67bfe56ef6ffd2943a
milldr <miller0daniel@gmail.com>
2024-05-28 13:46:15 -0700

    Remove region variable, update VPC ID variable description

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -1,7 +1,3 @@
-variable "region" {
-  type = string
-}
-
 variable "vpc_id" {
   type        = string
   description = "If set, the ID of the VPC in which the endpoint will be used. If not set, no VPC endpoint will be created."
