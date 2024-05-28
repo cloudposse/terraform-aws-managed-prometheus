06941fd2d4943b43519acdcbd6c403c4d95f2355
milldr <miller0daniel@gmail.com>
2024-05-28 13:43:51 -0700

    Update variable type formatting and alignment in examples

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -5,9 +5,9 @@ locals {
 module "managed_prometheus" {
   source = "../.."
 
-  allowed_account_id       = local.grafana_account_id
-  scraper_deployed         = true
-  vpc_id = var.vpc_id
+  allowed_account_id = local.grafana_account_id
+  scraper_deployed   = true
+  vpc_id             = var.vpc_id
 
   context = module.this.context
 }
--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -3,7 +3,7 @@ variable "region" {
 }
 
 variable "vpc_id" {
-  type = string
+  type        = string
   description = "If set, the ID of the VPC in which the endpoint will be used. If not set, no VPC endpoint will be created."
-  default = ""
+  default     = ""
 }
