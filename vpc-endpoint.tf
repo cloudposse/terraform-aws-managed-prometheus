6d48e7bc1611d2bb76eb9c3add7b2ba44fbb2568
milldr <miller0daniel@gmail.com>
2025-11-24 14:19:12 -0500

    Update service_name with current region id for VPC endpoint

--- vpc-endpoint.tf
+++ vpc-endpoint.tf
@@ -40,7 +40,7 @@ resource "aws_vpc_endpoint" "prometheus" {
   count = local.vpc_endpoint_enabled ? 1 : 0
 
   vpc_id            = var.vpc_id
-  service_name      = format("com.amazonaws.%s.aps-workspaces", data.aws_region.current.name)
+  service_name      = format("com.amazonaws.%s.aps-workspaces", data.aws_region.current.id)
   vpc_endpoint_type = "Interface"
 
   policy = module.vpc_endpoint_policy.json
